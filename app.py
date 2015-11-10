611ac0ba27c345f0474e176ab55e2b4c14a21198
Sally <sallybao29@gmail.com>
2015-11-09 21:15:24 -0500

    added new keys and url

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
     app.debug = True
+    app.secret_key = "69cce1c1daa03989"
     app.run(host = '0.0.0.0', port = 8000)
--- utils.py
+++ utils.py
@@ -16,8 +16,12 @@ def define(query):
     print r["results"][0]["senses"][0]["definition"][0]
     
 #takes: one word from definition
-#returns image url 
-def picture(word):
+#returns: image url 
+def get_pict(word):
+    url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=%s&format=json&media=photos&tags=%s&text=%s"
+    key = "e0e0c259e7e2a1f07f6c8e6a74579f12"
+    url = url % (key, word, word)
+    
 
     
 #takes: the search query
