d5027412aad0df1d182facb72f589477f2279e7e
Sally <sallybao29@gmail.com>
2015-11-09 23:57:49 -0500

    worked on define

--- utils.py
+++ utils.py
@@ -1,23 +1,48 @@
 import urllib2, json
 
 
-#takes the search query and appends to url
-#run through api
-#returns: the definition of the word (as a list or a string?)
-#what to do if there is more than one definition 
 def define(query):
-    url = "https://api.pearson.com:443/v2/dictionaries/ldoce5/entries?search=%s"
-    url = url % query
+
+    """
+    description: runs an api search on a word and returns the definition (there may be more than one)
+
+    params: query, a string
+
+    returns: definition, a list
+    """
+    
+    url = "https://api.pearson.com:443/v2/dictionaries/ldoce5/entries?search=%s&headword=%s"
+    url = url % (query, query)
     request = urllib2.urlopen(url)
     result = request.read()
     r = json.loads(result)
 
     #how to get a single definition
-    print r["results"][0]["senses"][0]["definition"][0]
+    #print r
+    #print r["results"][0]["senses"][0]["definition"][0]
     
-#takes: one word from definition
-#returns: image url 
+    defs = []
+    for res in r["results"]:
+        if res["headword"] == query:
+            try:
+                defs.append(res["senses"][0]["definition"][0])
+            except:
+                pass
+    print defs
+    return defs
+
+
+    
 def get_pict(word):
+
+    """
+    description: run a word first through an api search function, then through an info function to get url of corresponding photo
+
+    params: word, a string
+
+    returns: url, a string
+    """
+    
     #searching for photo
     url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=%s&format=json&media=photos&tags=%s&text=%s"
     key = "e0e0c259e7e2a1f07f6c8e6a74579f12"
@@ -39,5 +64,7 @@ def pictify(query):
 
 
 
-    
-define("cat")
+#Testing     
+define("stab")
+define("eviscerate")
+define("gallows")
